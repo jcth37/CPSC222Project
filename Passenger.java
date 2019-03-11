e2b6b3661f92b2d636ce122c67b9422cfba83633
jcth37 <36866540+jcth37@users.noreply.github.com>
2019-03-11 16:52:02 -0700

    Add files via upload

--- Passenger.java
+++ Passenger.java
@@ -3,5 +3,18 @@ package TrainSim;
 
 
 public class Passenger {
+    private Station dest; //destination
+    private Location current;
     
+    public Passenger(){
+        reset();
+    }
+    
+    public void reset(){
+        //sets destination and current
+    }
+    
+    public Station getDest(){
+        return dest;
+    }
 }
--- Station.java
+++ Station.java
@@ -2,4 +2,7 @@ package TrainSim;
 
 public class Station extends Location{
     
+    public Station(){
+        
+    }
 }

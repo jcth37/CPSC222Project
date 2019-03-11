6fb51d1e5cf1d75f65812ee0c82911f5f47c5856
jcth37 <36866540+jcth37@users.noreply.github.com>
2019-03-11 16:39:39 -0700

    Add files via upload

--- Train.java
+++ Train.java
@@ -3,10 +3,23 @@ package TrainSim;
 public class Train extends Thread{
     
     public static final int CAP = 6;//max capacity
-    //subject to change
-    private static int contains;
+    private static int nextID = 0;
+    private static int collisions = 0;
+    
+    private int contains;
+    private int id;
+    private Location loc;
+    private Passenger[] people;
+    //route?
     
     public Train(){
+        id = nextID;
+        nextID++;
         contains = 0;
+    }
+    
+    @Override
+    public void run(){
+        
     }
 }

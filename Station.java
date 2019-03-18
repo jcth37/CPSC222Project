f3b782b900b1e197e41cd96c5abae594cf5358d1
jcth37 <36866540+jcth37@users.noreply.github.com>
2019-03-18 09:13:12 -0700

    Add files via upload

--- Station.java
+++ Station.java
@@ -1,9 +1,27 @@
 package TrainSim;
 
+import java.util.ArrayList;
+import java.util.Random;
 public class Station extends Location{
+    private static final Random r = new Random();
+    private static final ArrayList<Station> allStations = new ArrayList();
+    private ArrayList<Passenger> p = new ArrayList();
+    
     public Station(){
-        
+        allStations.add(this);
+        init();
     }
     
+    private void init(){
+        //intended only to be called when initializing station.
+        for(int i = 0 ; i < 5 ; i++){
+            p.add(new Passenger(this));
+        }
+    }
     
+    public Station getRandomStation(){
+        //used by passengers to get their destination
+        int n = r.nextInt(allStations.size());
+        return allStations.get(n);
+    }
 }

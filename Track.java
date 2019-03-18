312e5ae846007aad6088745b70d5194dc39b48e8
Mitchell Labatiuk <48189020+CorgiFoxtrot@users.noreply.github.com>
2019-03-17 22:20:49 -0700

    Update Track.java

--- Track.java
+++ Track.java
@@ -1,13 +1,25 @@
-
 package TrainSim;
+
+// Update: -Added 'distance' variable; which currently has no way to be calculated.
+//         -Can return station one/two depending on specified train direction.
 
 public class Track extends Location{
     private Station one;
     private Station two;
+    private double distance;
     
     public Track(Station a, Station b){
         one = a;
         two = b;
+        // Need a way to determine distance.
+    }
+    
+    public double getDistance() {
+        return distance;
+    }
+    
+    public Station getStation(int direction) {
+        return direction <= 0 ? one : two;
     }
     
 }

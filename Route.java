cd7c2b16b48e7550871a0c8b6633be742153b429
Mitchell Labatiuk <48189020+CorgiFoxtrot@users.noreply.github.com>
2019-03-18 12:23:39 -0700

    Update and rename Tracks.java to Route.java

--- Route.java
+++ Route.java
@@ -5,23 +5,23 @@ package TrainSim;
 //          between stations
 
 import java.util.Random;
-public class Tracks {
-    private Track[] route;  //should have different name?
+public class Route {
+    private Track[] tracks;  //should have different name?
     
     public Tracks(Station... stations) {
         Random r = new Random();
         // This constructs array of tracks from ordered set of stations
         if (stations.length < 2)
             throw new RuntimeException("Need at least two stations!");
-        route = new Track[stations.length-1];
+        tracks = new Track[stations.length-1];
         for (int i = 0; i < stations.length-1; i++)
-            route[i] = new Track(stations[i], stations[i+1], r.nextDouble()*35);
+            tracks[i] = new Track(stations[i], stations[i+1], r.nextDouble()*35);
     }
     
     public Track getTrack(int i) {
-        if (i < 0 || i >= route.length)
+        if (i < 0 || i >= tracks.length)
             return null;
-        return route[i];
+        return tracks[i];
     }
     
 }

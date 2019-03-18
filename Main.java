909810b29781dea3d548548c9808a2c5ffb8a4d8
jcth37 <36866540+jcth37@users.noreply.github.com>
2019-03-18 13:52:02 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,7 +3,17 @@ package TrainSim;
 
 public class Main {
     public static void main(String[] args){
+        Station a = new Station();
+        Station b = new Station();
+        Station c = new Station();
+        Station d = new Station();
+        Route r = new Route(a, b, c);
+        Route r2 = new Route(c, d);
         
+        Train t1 = new Train(r);
+        Train t2 = new Train(r2);
+        
+        t1.start();
+        t2.start();
     }
-
 }
